0f4d6b1919e072f6f20e0b201b0e54e8c78b6dec
John Beale <7452231+jbeale1@users.noreply.github.com>
2022-12-04 23:15:16 -0800

    include Vref, Voffset as parameters

--- vsys.py
+++ vsys.py
@@ -1,12 +1,13 @@
 """
 # vsys.py : Read Pico Vsys voltage using ADC channel 3 (GPIO 29)
+# takes parameters ADC Vref and Voffset (ideally would be 3.3,0)
 # J.Beale 4-Dec-2022
 
 # Usage Example:
 import vsys
 from time import sleep, time
 
-vs = vsys.Vsys() # to read Pico Vsys voltage
+vs = vsys.Vsys(3.24,0.02) # to read Pico Vsys voltage
 while True:
     volts = vs.read()
     print(volts)
@@ -15,10 +16,10 @@ while True:
 
 from machine import Pin, ADC
 
-class Vsys():
-    def __init__(self):
-        self.vOffset = +0.027  # individually measured offset
-        self.Vref = 3.2335     # individually measured value of ADC Vref
+class Vsys:
+    def __init__(self, vref=3.234, voff=0.027):
+        self.vOffset = voff    # individually measured offset
+        self.Vref = vref       # individually measured value of ADC Vref
         Pin(29, Pin.IN)        # no pullup/down for pin 29 reading Vsys
         self.vsysCh = ADC(3)   # connects to Vsys
         self.ADCmax = 65535    # max reported by ADC().read_u16
@@ -27,4 +28,3 @@ class Vsys():
 
     def read(self):  # read vSys in volts
         return (self.vsysCh.read_u16() * self.vScale + self.vOffset)
-
